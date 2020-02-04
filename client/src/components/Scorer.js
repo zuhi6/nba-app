0ba5592cf7c6adc1b21036c4b00ea6b54b20e4b6
Michal Zajic <mzajic@gratex.com>
2020-02-04 10:49:06 +0100

    added some styling

--- client/src/components/Scorer.js
+++ client/src/components/Scorer.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Spinner, Media } from "react-bootstrap";
+import { Spinner, Media, Container, Row, Col } from "react-bootstrap";
 import axios from "axios";
 
 export default class Scorer extends Component {
@@ -34,13 +34,28 @@ export default class Scorer extends Component {
               width={254}
               height={182}
               className="mr-3"
+              style={{"border-radius": "20%","border":"1px solid black","box-shadow": "0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)"}}
               src={this.state.playerPhotoUrl}
               alt="Generic placeholder"
             />
             <Media.Body>
-              <h5>{this.props.player.name}</h5>
-              <h6>{this.props.player[this.props.property]}</h6>
-              <h4>{this.props.stats.fga}</h4>
+            <h5>{this.props.player.name}</h5>
+              <Container>
+                <Row className="justify-content-md-center">
+                  <Col xs lg="8">
+                    <p>{this.props.player[this.props.property]}</p>
+                    
+                  </Col>
+                  <Col xs lg="8">
+                  <p>{this.props.property}</p>
+                    
+                  </Col>
+               
+
+                </Row>
+              
+              </Container>
+            
             </Media.Body>
           </Media>
         )}
